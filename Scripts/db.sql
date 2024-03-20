765f212dfbdbb14cc97187190c2d8c50d7713216
Oliverio Rojas Sanchez <22620188@tlaxiaco.tecnm.mx>
2024-03-19 21:22:34 -0600

    Agregue las tablas de mi base de datos

--- Scripts/db.sql
+++ Scripts/db.sql
@@ -1,1 +1,32 @@
-create database Hotel;
+create database if not exists Hotel;
+create table if not exists Hotel.empleado(
+	ID_empleado INT NOT NULL auto_increment,
+    nombre varchar(50) not null,
+    fecha_contratacion varchar(50) NOT NULL,
+    rol enum('matutino','vespertino'),
+    PRiMARY KEY (id_empleado)
+    
+);
+
+CREATE TABLE if not exists Hotel.reserva (
+    id_reserva INT PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INT,
+    id_habitacion INT,
+    fecha_inicio DATE,
+    fecha_fin DATE,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estado_reserva VARCHAR(20),
+    metodo_pago VARCHAR(50),
+    precio_total DECIMAL(10, 2),
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
+    FOREIGN KEY (id_habitacion) REFERENCES Habitacion(id_habitacion)
+);
+
+CREATE TABLE if not exists Hotel.habitacion (
+    id_habitacion INT PRIMARY KEY AUTO_INCREMENT,
+    tipo_habitacion VARCHAR(50),
+    estado_disponibilidad VARCHAR(20),
+    precio_noche DECIMAL(10, 2),
+    capacidad INT,
+    descripcion TEXT
+);
